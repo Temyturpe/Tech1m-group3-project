2d3354fb6ae57eb250ad258ee5b78cd538b9159a
Nnaemeka <emekason128@gmail.com>
2023-03-10 23:26:51 +0100

    Added custom styles, configurations and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors : {
+      'primary': '#197EF2',
+      'neutral': '#7C7E80',
+      'black': '#1E1717',
+      'white': '#F8F4F4',
+      'error': '#FA0724',
+      'success': '#01A830'
+    },
     extend: {},
   },
   plugins: [],
