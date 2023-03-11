0f15d24aab58913304914dd13b4111f4efed45fd
Temitope <themyturpeokesanya@gmail.com>
2023-03-11 20:17:40 +0100

    dashboard skeleton and routes

---
+++ src/pages/Students.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Students = () => {
+  return (
+    <div className='w-full'>
+    <div className="content w-full flex">
+        <div className="left w-[20%]"></div>
+        <div className="right w-[80%] text-2xl font-semibold ">STUDENTS PAGE</div>
+    </div>
+</div>
+  )
+}
+
+export default Students
