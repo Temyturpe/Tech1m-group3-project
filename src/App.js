0df378d9762c7c92d7edad4c0431d93495bcef8e
Nnaemeka <emekason128@gmail.com>
2023-03-11 20:31:15 +0100

    icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import './assets/css/global.css';
-import './assets/fonts/airticulat/fonts/ArticulatCF-Bold.ttf';
-import './assets/fonts/airticulat/fonts/ArticulatCF-Heavy.ttf';
-import './assets/fonts/rubrik/Rubrik-Semibold.ttf';
-import './assets/fonts/rubrik/Rubrik.ttf';
 import Course from './components/Course';
 
 function App() {
