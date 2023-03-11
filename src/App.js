3191bd632394f3d0b1a010b18b327f09f3aa04b5
Nnaemeka <emekason128@gmail.com>
2023-03-11 18:23:53 +0100

    course page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import './assets/css/global.css';
+import './assets/fonts/airticulat/fonts/ArticulatCF-Bold.ttf';
+import './assets/fonts/airticulat/fonts/ArticulatCF-Heavy.ttf';
+import './assets/fonts/rubrik/Rubrik-Semibold.ttf';
+import './assets/fonts/rubrik/Rubrik.ttf';
+import Course from './components/Course';
+
 function App() {
   return (
     <div className="App text-3xl text-blue-700">
-     hello
+     <Course />
     </div>
   );
 }
