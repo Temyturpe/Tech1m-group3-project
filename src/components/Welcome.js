546ff67a8a75dd5bfbcc2ae8755cd02a3909c627
lolaakinrinsola <ololadeakinrinsola@gmail.com>
2023-03-11 21:48:59 +0100

    feat-loginupdated

---
+++ src/components/Welcome.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <h1>Welcome</h1>
+        <p>Get ready to explore the exciting world of
+technology and learn new skills that will help you shape your future.</p>
+        <button>
+            Let's Get Started
+        </button>
+        <small>Already have an account! <a href="#">Log in</a></small>
+      </div>
+    </div>
+  )
+}
+
+export default Welcome
